1de476772988145f125d26f65714e25176a8d5da
enzodelkirik <160311028+enzodelkirik@users.noreply.github.com>
2024-04-02 11:34:02 +0300

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        string? inputM = Console.ReadLine();
+        if (int.TryParse(inputM, out int M))
+        {
+            Console.Write("Введите значение N: ");
+            string? inputN = Console.ReadLine();
+            if (int.TryParse(inputN, out int N))
+            {
+                PrintNaturalNumbers(M, N);
+            }
+            else
+            {
+                Console.WriteLine("Некорректное значение N. Пожалуйста, введите целое число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректное значение M. Пожалуйста, введите целое число.");
+        }
+    }
+
+    static void PrintNaturalNumbers(int start, int end)
+    {
+        if (start <= end)
+        {
+            Console.WriteLine(start);
+            PrintNaturalNumbers(start + 1, end);
+        }
+    }
+}
