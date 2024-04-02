cfab12ff1a447fb26e7bae89cca6bec27a4e9c84
enzodelkirik <160311028+enzodelkirik@users.noreply.github.com>
2024-04-02 11:54:51 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.
+*/
+
+using System;
+
+class Program
+{
+    static void PrintArrayReverse(int[] arr, int index)
+    {
+        if (index < 0)
+            return;
+
+        Console.Write(arr[index] + " ");
+        PrintArrayReverse(arr, index - 1);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите размер массива:");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Console.WriteLine("Введите элементы массива:");
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Элемент {i + 1}: ");
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayReverse(array, size - 1);
+    }
+}
